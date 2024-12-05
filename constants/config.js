227528ac4591e0c33c80d7bf7c03786ca2083c1a
Anup-D-Dutta <codewithanup@gmail.com>
2024-12-05 15:27:49 +0530

    first commit

---
+++ constants/config.js
@@ -0,0 +1,13 @@
+const corsOptions ={
+    origin: [
+        "http://localhost:5173",
+        // "http://localhost:4173", 
+        // process.env.CLIENT_URL
+    ],
+    methods: ['GET','POST','PUT','DELETE'],
+    credentials: true, // Allow cookies
+}
+
+const MESSAGE_TOKRN = "message-Token"
+
+export{corsOptions, MESSAGE_TOKRN}
---
+++ middlewares/auth.js
@@ -0,0 +1,58 @@
+import { ErrorHandler } from "../utils/utility.js";
+import jwt from "jsonwebtoken"
+import { TryCatch } from "./error.js";
+import { MESSAGE_TOKRN } from "../constants/config.js";
+import { User } from '../models/user.js';
+
+
+const isAuthenticated = TryCatch(async (req, res, next) => {
+
+    const token = req.cookies[MESSAGE_TOKRN];
+
+    if (!token)
+
+        return next(new ErrorHandler("Please login to access this route", 401));
+
+    const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decodedData._id;
+
+    next();
+});
+
+// here one function asminOnly
+
+
+const socketAuthenticator = async (err, socket, next) => {
+
+    try {
+
+        if (err)
+            return next(err);
+
+        const authToken = socket.request.cookies[MESSAGE_TOKRN];
+
+        if (!authToken)
+            return next(new ErrorHandler("Please login to access this route", 401));
+
+        const decodedData = jwt.verify(authToken, process.env.JWT_SECRET)
+
+        const user = await User.findById(decodedData._id);
+
+        if (!user)
+            return next(new ErrorHandler("Please login to access this route", 401));
+
+
+        socket.user = user;
+
+        return next();
+
+    } catch (error) {
+        console.log(error)
+        return next(new ErrorHandler("Please login to access this route", 401))
+
+    }
+}
+
+export { isAuthenticated, socketAuthenticator }
+
