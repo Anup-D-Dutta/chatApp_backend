55e2caee4e31b5546d0c36c9819f0366a6ec43b2
Anup-D-Dutta <codewithanup@gmail.com>
2024-12-05 20:28:54 +0530

    fixed

--- constants/config.js
+++ constants/config.js
@@ -1,6 +1,7 @@
 const corsOptions ={
     origin: [
-        "https://chat-app-front-end-kgrz.vercel.app",
+        "http://localhost:5173",
+        "http://localhost:4173", 
         process.env.CLIENT_URL
     ],
     methods: ['GET','POST','PUT','DELETE'],
