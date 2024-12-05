de5ecdc4bb131ba829624795cd989df96a85c05f
Anup-D-Dutta <codewithanup@gmail.com>
2024-12-05 18:36:55 +0530

    fixed

--- constants/config.js
+++ constants/config.js
@@ -1,36 +1,14 @@
-// const corsOptions ={
-//     origin: [
-//         "http://localhost:5173",
-//         "http://localhost:4173", 
-//         process.env.CLIENT_URL
-//     ],
-//     methods: ['GET','POST','PUT','DELETE'],
-//     credentials: true, // Allow cookies
-// }
-
-// const MESSAGE_TOKRN = "message-Token"
-
-// export{corsOptions, MESSAGE_TOKRN}
-
-
-const corsOptions = {
-    origin: (origin, callback) => {
-        const allowedOrigins = [
-            "http://localhost:5173",
-            "http://localhost:4173",
-            process.env.CLIENT_URL
-        ];
-        if (allowedOrigins.includes(origin) || !origin) {
-            // Allow the request if origin matches or it's a non-browser request (no origin)
-            callback(null, true);
-        } else {
-            // Block the request
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+const corsOptions ={
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:4173", 
+        'https://chat-app-front-end-kgrz.vercel.app',
+        process.env.CLIENT_URL
+    ],
+    methods: ['GET','POST','PUT','DELETE'],
     credentials: true, // Allow cookies
-};
+}
+
 const MESSAGE_TOKRN = "message-Token"
 
 export{corsOptions, MESSAGE_TOKRN}
