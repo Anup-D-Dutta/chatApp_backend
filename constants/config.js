a2c3eb09552677d8b6584e517771b287ec084a3d
Anup-D-Dutta <codewithanup@gmail.com>
2024-12-10 21:23:12 +0530

    fixed

--- constants/config.js
+++ constants/config.js
@@ -1,8 +1,7 @@
 const corsOptions ={
     origin: [
         "http://localhost:5173",
-        'https://chat-app-frontend-blue-pi.vercel.app/'
-        // process.env.CLIENT_URL,
+        process.env.CLIENT_URL,
     ],
     methods: ['GET','POST','PUT','DELETE'],
     credentials: true, // Allow cookies
