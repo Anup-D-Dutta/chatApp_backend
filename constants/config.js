1fb5c5d5e7f8a84a891f4e1a1630632f109b56d5
Anup-D-Dutta <codewithanup@gmail.com>
2025-03-12 20:50:57 +0530

    fixed

--- constants/config.js
+++ constants/config.js
@@ -16,6 +16,7 @@
 const allowedOrigins = [
     "http://localhost:5174",
     "http://localhost:5173",
+    "https://chat-app-frontend-livid-eight.vercel.app/login"
 ];
 
 if (process.env.CLIENT_URL) {
