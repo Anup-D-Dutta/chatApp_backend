8dca2cb59e4386f7e4a8689342d32389292c0b8a
Anup-D-Dutta <codewithanup@gmail.com>
2025-03-12 15:42:05 +0530

    fixed

--- constants/config.js
+++ constants/config.js
@@ -1,12 +1,13 @@
 const corsOptions ={
     origin: [
         "http://localhost:5174",
+        "http://localhost:5173",
+        process.env.CLIENT_URL,
     ],
     methods: ['GET','POST','PUT','DELETE'],
     credentials: true, // Allow cookies
     allowedHeaders: ['Content-Type', 'Authorization'], // Headers to allow
 }
-
 
 const MESSAGE_TOKRN = "message-Token"
 
