babc6a19da4efc49aac727a41d95246187f7f4a4
Anup-D-Dutta <codewithanup@gmail.com>
2025-08-07 13:54:10 +0530

    fix cors

--- constants/config.js
+++ constants/config.js
@@ -3,7 +3,7 @@ const corsOptions = {
     origin: [
         "http://localhost:5174",
         "http://localhost:5173",
-        process.env.CLIENT_URL,
+        "https://talksynccc.netlify.app/login"
     ].filter(Boolean), // Removes undefined values
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, // Allow cookies
