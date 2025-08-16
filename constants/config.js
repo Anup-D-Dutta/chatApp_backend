fe70297c11eccd40a55c6ee92b8ed80a2550bbf2
Anup-D-Dutta <codewithanup@gmail.com>
2025-08-16 14:09:02 +0530

    Fix URL

--- constants/config.js
+++ constants/config.js
@@ -3,10 +3,8 @@ const corsOptions = {
     origin: [
         "http://localhost:5174",
         "http://localhost:5173",
-        "https://talksynccc.netlify.app/login",
-        "https://talksynccc.netlify.app",
-        "https://talksyncc.netlify.app/login",
-        "https://talksyncc.netlify.app"
+       "https://anup-talksync.netlify.app/login",
+       "https://anup-talksync.netlify.app"
     ].filter(Boolean), // Removes undefined values
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, // Allow cookies
